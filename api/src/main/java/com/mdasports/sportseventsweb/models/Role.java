0c1523e22feaf0dec878f76ddb122ea92c6e0015
Alejandro Perdomo González <apergot95@gmail.com>
2020-04-20 12:29:59 +0100

    #MDA-2 Role entity creation to add roles to users, we will thank this later.

---
+++ api/src/main/java/com/mdasports/sportseventsweb/models/Role.java
@@ -0,0 +1,12 @@
+package com.mdasports.sportseventsweb.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+}
