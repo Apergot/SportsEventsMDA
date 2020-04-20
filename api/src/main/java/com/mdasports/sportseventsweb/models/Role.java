e1b16f1bc0ce5c281df2a6f42a273a62bb551a11
Alejandro Perdomo González <apergot95@gmail.com>
2020-04-20 12:34:19 +0100

    #MDA-2 Role methods, getters and setters.

--- api/src/main/java/com/mdasports/sportseventsweb/models/Role.java
+++ api/src/main/java/com/mdasports/sportseventsweb/models/Role.java
@@ -14,5 +14,21 @@ public class Role implements Serializable {
     @Column(unique = true, length = 20)
     private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private static final Long serialVersionUID = 1L;
 }
