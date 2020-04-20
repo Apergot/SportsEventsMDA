c83dc8296efecee3d22e980ebd5ea058936d6e06
Alejandro Perdomo González <apergot95@gmail.com>
2020-04-20 12:33:16 +0100

    #MDA-2 Role params

--- api/src/main/java/com/mdasports/sportseventsweb/models/Role.java
+++ api/src/main/java/com/mdasports/sportseventsweb/models/Role.java
@@ -1,12 +1,18 @@
 package com.mdasports.sportseventsweb.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "roles")
 public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String name;
 
     private static final Long serialVersionUID = 1L;
 }
