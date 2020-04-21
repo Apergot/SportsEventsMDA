a4ca4ac528877415bfa57ad1d919691da0ab31ac
diegohawkins21 <diegohawkins21@gmail.com>
2020-04-21 10:12:23 +0100

    #MDA-3 Added the implementation of page users list in backend

--- api/src/main/java/com/mdasports/sportseventsweb/controllers/UserRestController.java
+++ api/src/main/java/com/mdasports/sportseventsweb/controllers/UserRestController.java
@@ -3,14 +3,16 @@ package com.mdasports.sportseventsweb.controllers;
 import com.mdasports.sportseventsweb.models.entities.User;
 import com.mdasports.sportseventsweb.models.services.IAdminUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
@@ -18,9 +20,17 @@ public class UserRestController {
     @Autowired
     private IAdminUsersService iAdminUsersService;
 
-    @GetMapping("users")
+    @GetMapping("/users")
     public List<User> index(){
         return iAdminUsersService.findAll();
     }
+
+    @GetMapping("/users/page/{page}")
+    public Page <User> index(@PathVariable Integer page){
+        Pageable pageable = PageRequest.of(page, 5);
+        return iAdminUsersService.findAll(pageable);
+    }
+
+
 
 }
