f6c60c519534ba12feed697ccc8b3f2d1585aefd
diegohawkins21 <diegohawkins21@gmail.com>
2020-04-21 10:40:44 +0100

    #MDA-3 Added the implementation of show users in backend

--- api/src/main/java/com/mdasports/sportseventsweb/controllers/UserRestController.java
+++ api/src/main/java/com/mdasports/sportseventsweb/controllers/UserRestController.java
@@ -3,15 +3,21 @@ package com.mdasports.sportseventsweb.controllers;
 import com.mdasports.sportseventsweb.models.entities.User;
 import com.mdasports.sportseventsweb.models.services.IAdminUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -31,6 +37,23 @@ public class UserRestController {
         return iAdminUsersService.findAll(pageable);
     }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> show(@PathVariable Long id){
+        User user = null;
+        Map<String, Object> map = new HashMap<>();
+        try {
+            user = iAdminUsersService.findById(id);
+        } catch (DataAccessException e) {
+            map.put("message", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+        if (user == null) {
+            map.put("message", "User id ".concat(id.toString()).concat("user does not exists"));
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
+    }
 
 
 }
