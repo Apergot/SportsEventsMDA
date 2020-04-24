43d137b5f9aba93e74a1a843f7ef7b6be664eef6
miguelberciano <miguelangelbercianorodriguez@gmail.com>
2020-04-24 21:00:06 +0100

    #MDA-5 update, create and getbyid

---
+++ web/src/app/user-form/user-form.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from "@angular/core";
+import { User } from "../users/user";
+import { Role } from "../users/role";
+import { UserService } from "../users/user.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import swal from "sweetalert2";
+
+@Component({
+  selector: "app-user-form",
+  templateUrl: "./user-form.component.html",
+  styleUrls: ["./user-form.component.css"],
+})
+export class UserFormComponent implements OnInit {
+  user: User = new User();
+  title: string = "Create User";
+
+  errors: string[];
+
+  roles: Role[] = [
+    { id: 1, name: "ROLE_USER" },
+    { id: 2, name: "ROLE_ADMIN" },
+  ];
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let id = +params.get("id");
+      if (id) {
+        this.title = "Update user";
+        this.userService.getUser(id).subscribe((user) => {
+          this.user = user;
+          console.log(user);
+        });
+      }
+    });
+  }
+
+  create() {
+    this.userService.create(this.user).subscribe(
+      (user) => {
+        this.router.navigate(["/users"]);
+        swal.fire(
+          "New user",
+          `User ${user.username} has been created successfully`,
+          "success"
+        );
+      },
+      (err) => {
+        this.errors = err.error.errors as string[];
+        console.error("Backend code error: " + err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
+
+  update() {
+    this.userService.update(this.user).subscribe(
+      (user) => {
+        this.router.navigate(["/users"]);
+        swal.fire(
+          "User updated",
+          `User ${user.username} has been updated successfully`,
+          "success"
+        );
+      },
+      (err) => {
+        this.errors = err.error.errors as string[];
+        console.error("Backend code error: " + err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
+}
