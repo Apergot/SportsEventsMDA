d99a3b5067aba84552702df73fdac196869c468b
diegohawkins21 <diegohawkins21@gmail.com>
2020-05-07 17:25:18 +0100

    #MDA-9 Starting 07/05/2020

--- api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
+++ api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
@@ -3,11 +3,11 @@ package com.mdasports.sportseventsweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*String1 7/05/2020 start*/
 @SpringBootApplication
 public class SportseventswebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SportseventswebApplication.class, args);
     }
-
 }
