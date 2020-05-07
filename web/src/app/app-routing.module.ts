6b72abdfb4e8e9263051f04c34b7e4e85dc28053
miguelberciano <miguelangelbercianorodriguez@gmail.com>
2020-05-07 20:44:08 +0100

    #MDA-8 VISTA form login

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { UsersComponent } from "./users/users.component";
 import { UserFormComponent } from "./user-form/user-form.component";
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "users", component: UsersComponent },
   { path: "users/form", component: UserFormComponent },
   { path: "users/form/:id", component: UserFormComponent },
+  { path: "login", component: LoginComponent}
 ];
 
 @NgModule({
--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../users/user';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  user: User = null;
+  title = 'Login';
+
+  errors: string[];
 
   constructor() { }
 
