5d784ebd12dfd8cce06d190f4b732d36cc372ccf
Alejandro Perdomo González <apergot95@gmail.com>
2020-05-08 13:50:43 +0100

    #MDA-7 now we have jwt implemented, just need to specify permissions for each route and integrating it.

--- api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
+++ api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportseventswebApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SportseventswebApplication.class, args);
     }
