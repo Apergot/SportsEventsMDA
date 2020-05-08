6ef0d3e85e11b94ca93f8039c069486702e92f6c
Alejandro Perdomo González <apergot95@gmail.com>
2020-05-08 12:20:48 +0100

    #MDA-7 adding Spring Security Config to use Spring Security on API mode

--- api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
+++ api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportseventswebApplication {
-    /*Sprint 1 08/05/2020*/
     public static void main(String[] args) {
         SpringApplication.run(SportseventswebApplication.class, args);
     }
