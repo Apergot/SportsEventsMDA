3a5ea67d3eb736d88d040a1119bb90f527e92fa6
Alejandro Perdomo González <apergot95@gmail.com>
2020-05-12 17:36:08 +0100

    #MDA-7 just finished reading spring security docs and learnt how to implement route security, just need @Secured and roles.

--- api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
+++ api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportseventswebApplication {
-    /*sprint 1 12/05/2020*/
     public static void main(String[] args) {
         SpringApplication.run(SportseventswebApplication.class, args);
     }
