4be2a99f80b0276ed64139fbc5f11deb072bb936
Alejandro Perdomo González <apergot95@gmail.com>
2020-05-12 18:45:50 +0100

    #MDA-7 testing user jpa repository creation and retrieving

--- api/src/test/java/com/mdasports/sportseventsweb/UserDaoTest.java
+++ api/src/test/java/com/mdasports/sportseventsweb/UserDaoTest.java
@@ -1,11 +1,18 @@
 package com.mdasports.sportseventsweb;
 
 import com.mdasports.sportseventsweb.models.dao.IUserDao;
+import com.mdasports.sportseventsweb.models.entities.Role;
+import com.mdasports.sportseventsweb.models.entities.User;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -17,5 +24,31 @@ public class UserDaoTest {
     @Autowired
     private IUserDao userDao;
 
-    //write test cases here
+    @Test
+    public void whenFindByUsername_thenReturnUser() {
+
+        //given
+        User apergot = new User();
+        apergot.setFirstname("Alejandro");
+        apergot.setLastname("Perdomo");
+        apergot.setUsername("Apergot");
+        apergot.setEnabled(true);
+        apergot.setPassword("$2a$10$Q2BWjW7p/4Dnxsz1LaTj3O/N0L6mJ/mTH0Y2HT4JMZDByRE7DEdmy");
+        apergot.setEmail("apergot95@gmail.com");
+        Role user_role = new Role();
+        user_role.setId(1L);
+        user_role.setName("ROLE_USER");
+        Role admin_role = new Role();
+        admin_role.setId(2L);
+        admin_role.setName("ROLE_ADMIN");
+        apergot.setRoles(Arrays.asList(user_role, admin_role));
+        entityManager.persist(apergot);
+        entityManager.flush();
+
+        //when
+        User found = userDao.findByUsername(apergot.getUsername());
+
+        //then
+        assertThat(found.getFirstname()).isEqualTo(apergot.getFirstname());
+    }
 }
