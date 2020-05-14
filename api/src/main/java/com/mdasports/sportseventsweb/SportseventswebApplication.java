320369d7b51a89b685ff673a25ecc53783804e87
diegohawkins21 <diegohawkins21@gmail.com>
2020-05-14 17:16:36 +0100

    Starting work 14-25-2020

--- api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
+++ api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
@@ -3,7 +3,7 @@ package com.mdasports.sportseventsweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/*Spring 1 14-05-2020*/
 @SpringBootApplication
 public class SportseventswebApplication {
     public static void main(String[] args) {
