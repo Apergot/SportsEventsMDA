1d8524493ee4d085134538d50ce86640b6bea06d
Alejandro Perdomo González <apergot95@gmail.com>
2020-05-21 11:40:40 +0100

    #MDA-19 Added mail configuration for using google smtp

---
+++ api/src/main/java/com/mdasports/sportseventsweb/configuration/EmailConfiguration.java
@@ -0,0 +1,46 @@
+package com.mdasports.sportseventsweb.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = {"com.mdasports.sportseventsweb.mail"})
+public class EmailConfiguration {
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(mailHost);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "false");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
