1a139e8ac04041468d73e92152588d919415677a
Alejandro Perdomo <apergot95@gmail.com>
2020-05-27 14:19:36 +0100

    Starting out today's work

--- api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
+++ api/src/main/java/com/mdasports/sportseventsweb/SportseventswebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*Sprint 2 26/05/2020*/
 @SpringBootApplication
 public class SportseventswebApplication {
-
+    /*Sprint 2 27-05-2020*/
     public static void main(String[] args) {
         SpringApplication.run(SportseventswebApplication.class, args);
     }
