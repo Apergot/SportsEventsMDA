fa5c8891005f296cea022d769f5e9f1cb31a7558
Juan Kevin Trujillo <me@juankevintrujillo.com>
2020-05-29 18:39:12 +0100

    #MDA-14 Added more information to the public rivalries view

--- web/src/app/rivalries/site-rivalries/site-rivalries.component.ts
+++ web/src/app/rivalries/site-rivalries/site-rivalries.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Rivalry} from '../rivalry';
 import {RivalryService} from '../rivalry.service';
+import {Role} from '../../users/role';
 
 @Component({
   selector: 'app-site-rivalries',
@@ -10,10 +11,16 @@ import {RivalryService} from '../rivalry.service';
 export class SiteRivalriesComponent implements OnInit {
   rivalries: Rivalry[];
 
-  constructor(private rivalryService: RivalryService) { }
+  constructor(private rivalryService: RivalryService) {
+  }
 
   ngOnInit(): void {
     this.rivalryService.getRivalries().subscribe((rivalries) => (this.rivalries = rivalries));
+  }
+
+  displayPorcentage(capacity: number, enrolled: number) {
+    const porcentage = Math.round(((enrolled * 100) / capacity));
+    return porcentage + '%';
   }
 
 }
