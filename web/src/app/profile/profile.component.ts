450ebf8aef5a390d48860a38e8c52d412ad884b4
Juan Kevin Trujillo <me@juankevintrujillo.com>
2020-05-29 17:50:02 +0100

    #MDA-16 Small change in styles

--- web/src/app/profile/profile.component.ts
+++ web/src/app/profile/profile.component.ts
@@ -7,6 +7,7 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   user: User;
